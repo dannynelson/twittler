d5d721570def0937cb3950010edb5ba015c1e2de
Daniel Nelson <danielnelsonguitar@gmail.com>
2013-10-08 12:42:35 -0700

    created link to view user tweets

--- display.js
+++ display.js
@@ -7,9 +7,6 @@ $(document).ready(function(){
   var $stream = $('.stream');
   $stream.html('');
 
-
-
-
   //display all
   var displayAllTweets = function() {
     $stream.empty();
@@ -20,49 +17,21 @@ $(document).ready(function(){
       $tweet.html(
         "<div>" + 
           //link to user page, with username variable
-          "<a href=\"/user.html$\"" + tweet.user + ">" +
+          "<a href=\"user.html?username=" + tweet.user + "\">" +
             tweet.user +
           "</a>" +
         "</div>" +
-        "<div>@" + tweet.user + ": " + tweet.message + "</div>" +
-        "<div>" + tweet.created_at + "</div><br>"
+        "<div>" + tweet.message + "</div>" +
+        "<div>" + tweet.created_at + "</div>" +
+        "<br>"
       );
       $tweet.appendTo($stream);
       index -= 1;
     }
-
-    // setTimeout(displayAllTweets, 1000); //doesn't allow me to click on it while this is running
+    setInterval(displayAllTweets, 1000); //doesn't allow me to click on it while this is running
   }
 
   displayAllTweets();
-
-  //how to get username when clicked?
-  var displayUserTweets =  function(username) {
-    $stream.empty();
-    var index = streams.users[username].length - 1;
-    while(index >= 0){
-      var tweet = streams.users[username][index];
-      var $tweet = $('<div class="tweet"></div>');
-      $tweet.html(
-        "<div class=\"user-link\">" + tweet.user + "</div>" +
-        "<div>@" + tweet.user + ": " + tweet.message + "</div>" +
-        "<div>" + tweet.created_at + "</div><br>"
-      );
-      $tweet.appendTo($stream);
-      index -= 1;
-    }
-
-    setTimeout(function() {displayUserTweets(username);}, 1000);
-
-
-  }
-
-  $('.user-link').click(function() {
-    alert("Item was clicked");
-    var username = $(this).text();
-    // displayUserTweets(username);
-    setInterval(displayUserTweets(username), 1000);
-  })
 
   //update every second
 
