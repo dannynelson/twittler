6b80f906a11d26d8324eac66ddb2af28d55e317b
Daniel Nelson <danielnelsonguitar@gmail.com>
2013-10-08 12:45:20 -0700

    finished basic functional requirements

---
+++ display_user_tweets.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  // $variable is convention to ditinguish jquery objects stored in vars
+  var $body = $('body');
+    //to retrieve URL variable
+  function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+  }
+
+  var username = getUrlVars()["username"];
+  $('#tweet-title').html("<b>" + username + " Tweets </b>");
+
+  var $stream = $('.stream');
+  $stream.html('');
+
+  //how to get username when clicked?
+  var displayUserTweets =  function() {
+    $stream.empty();
+    var index = streams.users[username].length - 1;
+    while(index >= 0){
+      var tweet = streams.users[username][index];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.html(
+        "<div>" + 
+          //link to user page, with username variable
+          "<a href=\"user.html?username=" + tweet.user + "\">" +
+            tweet.user +
+          "</a>" +
+        "</div>" +
+        "<div>" + tweet.message + "</div>" +
+        "<div>" + tweet.created_at + "</div>" +
+        "<br>"
+      );
+      $tweet.appendTo($stream);
+      index -= 1;
+    }
+
+    setTimeout(function() {displayUserTweets(username);}, 1000);
+  }
+
+  displayUserTweets();
+
+
+});
