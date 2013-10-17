0ab4e90fe2dd3b024538ff21d1760759a95fb099
Daniel Nelson <danielnelsonguitar@gmail.com>
2013-10-17 09:13:26 -0700

    added compose tweet functionality

--- display.js
+++ display.js
@@ -1,3 +1,10 @@
+
+var visitor = prompt("Please enter your name:");
+sessionStorage.visitor = visitor;
+streams.users[visitor] = [];
+sessionStorage.streams = streams.users[visitor];
+
+
 $(document).ready(function(){
   // $variable is convention to ditinguish jquery objects stored in vars
   var $body = $('body');
@@ -6,6 +13,11 @@ $(document).ready(function(){
 
   var $stream = $('.stream');
   $stream.html('');
+
+  $('.compose').on('click', function() {
+    var message = prompt("What's happening?");
+    writeTweet(message);
+  })
 
   //display all
   var displayAllTweets = function() {
@@ -27,7 +39,7 @@ $(document).ready(function(){
       $tweet.appendTo($stream);
       index -= 1;
     }
-    displayAllTweets, 1000; //doesn't allow me to click on it while this is running
+    setInterval(displayAllTweets, 1000); //doesn't allow me to click on it while this is running
   }
 
   displayAllTweets();
