0297314a9ca71af224373ed41411db1258c56929
Daniel Nelson <danielnelsonguitar@gmail.com>
2013-10-17 11:35:29 -0700

    added parser for hashtags

--- display.js
+++ display.js
@@ -10,17 +10,37 @@ streams.users[visitor] = [];
 
 $(document).ready(function(){
   // $variable is convention to ditinguish jquery objects stored in vars
-  var $body = $('body');
-  // this replaces the ENTIRE body with nothing
-  // $body.html(''); 
-
+  
   var $stream = $('.stream');
+  var hashtags = [];
   $stream.html('');
+
+  var searchForHashtags = function(message) {
+    words = message.split(' ');
+    words = _(words).map(function(word) {
+      if (word.charAt(0) === "#") {
+        if (!_(hashtags).contains(word)) hashtags.push(word);
+        return '<a href="#">' + word + '</a>';
+      } else {
+        return word;
+      }
+    })
+    return words.join(' ');
+  }
 
   $('.compose').on('click', function() {
     var message = prompt("What's happening?");
     writeTweet(message);
   })
+
+  var displayAllHashtags = function() {
+    var hashtagsHTML = '<div><strong>Trends</div></strong>';
+    _(hashtags).each(function(hashtag) {
+      hashtagsHTML += '<div><a href="#">' + hashtag + '</a></div>';
+    })
+    $('.hashtags').empty().html(hashtagsHTML);
+    setInterval(displayAllHashtags, 5000);
+  }
 
   //display all
   var displayAllTweets = function() {
@@ -32,22 +52,24 @@ $(document).ready(function(){
       $tweet.html(
         '<div class="list">' + 
           //link to user page, with username variable
-          "<a href=\"user.html?username=" + tweet.user + "\">" +
+          '<a href="user.html?username="' + tweet.user + '"><strong>' +
             tweet.user +
-          "</a>" +
-          "<div>" + tweet.message + "</div>" +
-          '<div>' + tweet.created_at + '</abbr>' +
+          "</strong></a>" +
+          "<div>" + searchForHashtags(tweet.message) + "</div>" +
+          '<div class="timestamp">' + tweet.created_at + '</div>' +
         "</div>"
       );
       $tweet.appendTo($stream);
       index -= 1;
     }
-    setInterval(displayAllTweets, 1000); //doesn't allow me to click on it while this is running
+    setInterval(displayAllTweets, 3000); //doesn't allow me to click on it while this is running
   }
 
   displayAllTweets();
+  displayAllHashtags();
 
-  //update every second
+  //parse tweet for hashtags
+  //TODO: set hashtag link
 
 
 });
